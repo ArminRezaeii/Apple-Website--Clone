c8714dd4df121a10d4855e0dbd2c8f12005f0e2a
ArminRezaei <arminrezaei53@gmail.com>
2024-03-21 20:13:21 +0330

    title bug fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Iphone 15",
+  title: "Apple",
   description: "Apple website clone",
 };
 
