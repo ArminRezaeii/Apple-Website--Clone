e3ec3e1ff26bbd19101612fc3c5479e313c5af7b
ArminRezaei <arminrezaei53@gmail.com>
2024-03-25 00:04:59 +0330

    Lazy Loading added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Apple",
-  description: "Apple website clone",
+  title: "next js",
+  description: "next js",
 };
 
 export default function RootLayout({
